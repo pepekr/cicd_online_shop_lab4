2e2542c1d472ff7860631ff2e6f87a882f760589
Vladyslav Husenko <qmd4325621@gmail.com>
2025-05-27 20:06:09 +0300

    feat: added register view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.contrib.auth import login, logout
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from users.forms.CustomCreationForm import CustomUserCreationForm
+from users.forms.EmailLoginForm import EmailLoginForm
 
-# Create your views here.
+
+def register_view(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            login(request, form.save())
+            return redirect("/")
+    else:
+        form = CustomUserCreationForm()
+    return render(request, "registration.html", {"form": form})
