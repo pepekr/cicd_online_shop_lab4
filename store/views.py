0d164ac5b0016835e25941fbb5fe5f566609794c
pepekr <143782721+pepekr@users.noreply.github.com>
2025-05-27 21:02:10 +0300

    feat: added home view

--- store/views.py
+++ store/views.py
@@ -1,3 +1,25 @@
+import random
 from django.shortcuts import render
+from store.models import Product
 
-# Create your views here.
+
+def home(request):
+    selected_category = request.GET.get("category")
+    categories = Product.objects.values_list("category", flat=True).distinct()
+
+    # Filtered products if a category is selected
+    category_products = None
+    if selected_category:
+        category_products = Product.objects.filter(category=selected_category)
+
+    # Grab 20 random products
+    all_products = list(Product.objects.all())
+    random_products = random.sample(all_products, min(20, len(all_products)))
+
+    return render(request, "home.html", {
+        "user_name": request.user.username,
+        "categories": categories,
+        "selected_category": selected_category,
+        "category_products": category_products,
+        "random_products": random_products,
+    })
