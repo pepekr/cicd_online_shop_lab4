b4187310bd2ba2433bbb844adaa45c2356489867
pepekr <143782721+pepekr@users.noreply.github.com>
2025-05-27 20:52:23 +0300

    Update settings.py

--- cicd_online_shop_lab4/settings.py
+++ cicd_online_shop_lab4/settings.py
@@ -83,10 +83,9 @@ WSGI_APPLICATION = 'cicd_online_shop_lab4.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
 }
 
 
